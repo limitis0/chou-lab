34a51aa1e4249b1d4e121d20758f249ec68b04d1
leetw131 <leetw@jgallop.com>
2024-01-17 17:34:36 +0800

    adjust photo size

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -13,10 +13,7 @@ function About() {
     <path d="M31.76 18.58L24 26.34L16.24 18.58C15.46 17.8 14.2 17.8 13.42 18.58C12.64 19.36 12.64 20.62 13.42 21.4L22.6 30.58C23.38 31.36 24.64 31.36 25.42 30.58L34.6 21.4C35.38 20.62 35.38 19.36 34.6 18.58C33.82 17.82 32.54 17.8 31.76 18.58Z" fill="white" />
   </svg>);
 
-  const handleEnter = () => {
-    console.log('enter');
-    setIsShowEnterScreen(false);
-  };
+  const handleEnter = () => setIsShowEnterScreen(false);
 
   const getPageData = async () => {
     const rtAboutData = await getSingleDoc('ABOUT', 'ABOUT_CONTENT');
